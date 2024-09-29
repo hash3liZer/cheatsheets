dc1c86b91b90bda17cf5a0072a98a1e48743506e
hash3liZer <me@shameerkashif.me>
2024-09-30 00:58:32 +0500

    Updated icons

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
         title: "Cheatsheets | hash3liZer",
         logo: {
           alt: "Logo",
-          src: "img/logo.svg",
+          src: "img/logo.png",
         },
 
         items: [
@@ -98,7 +98,7 @@ const config = {
       },
 
       prism: {
-        theme: lightCodeTheme,
+        theme: darkCodeTheme,
         darkTheme: darkCodeTheme,
       },
     }),
